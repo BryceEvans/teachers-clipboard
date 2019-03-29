f2fa018b4bbe2b9884e230b695a60516756a99ae
BryceEvans <evans.bryce@gmail.com>
2019-03-29 12:35:22 -0600

    Adds state, count, and conditional rendering.

--- client/src/components/StudentCards/BottomRow.js
+++ client/src/components/StudentCards/BottomRow.js
@@ -4,24 +4,37 @@ import { Icon } from 'semantic-ui-react';
 class BottomRow extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      count: 0,
+
+      countItems: {
+        exchange: 0,
+        eye: 0,
+        phone: 0
+      }
+    }
     this.icons = [
       { name: 'user' },
       { name: 'exchange' },
       { name: 'eye' },
+      { name: 'phone' }
       // { name: 'question' }
     ]
   }
 
   clickHandler = (e) => {
-    alert("Item clicked!");
+    // alert("Item clicked!");
+    this.setState({ count: this.state.count + 1 })
   }
 
   render() { 
     return ( 
       <div style={{display: "flex", justifyContent: "space-between"}}>
         {this.icons.map((icon, ind) => (
-          <Icon name={icon.name} onClick={this.clickHandler} style={{cursor:'pointer'}}/>
+          <div style={{display: "flex", flexDirection: "column"}}>
+            <Icon name={icon.name} onClick={this.clickHandler} style={{cursor:'pointer'}}/>
+            { this.state.count > 0 ? <p>{this.state.count}</p> : null }
+          </div>
         ))}
       </div>
     );

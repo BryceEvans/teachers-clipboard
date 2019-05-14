0d1b234c4236649dcc743f5e3e11cb951ecb33bd
Haag <kkhaag@gmail.com>
2019-05-14 15:16:39 -0600

    Created todo list

--- server/person.service.js
+++ server/person.service.js
@@ -4,51 +4,39 @@ require('./mongoConnect').connect()
 
 function getPeople(req, res) {
     const docquery = Person.find({}).read(ReadPreference.NEAREST);
+    
     docquery
-    .exec()
-    .then(people => {
-        res.status(200).json(people)
-    })
-    .catch(error => {
-        res.status(500).send(error)
-        return
-    })
+        .exec()
+        .then(people => {
+            res.status(200).json(people)
+        })
+        .catch(error => {
+            res.status(500).send(error)
+            return
+        })
 }
 
 function postPerson(req, res) {
     const originalPerson = { id, first_name, last_name} = req.body;
-    console.log('originalPerson:', originalPerson)
+      console.log('originalPerson:', originalPerson)
     const person = new Person(originalPerson);
     person.save(error => {
-      if (checkServerError(res, error)) return;
-      res.status(201).json(person);
+        if (checkServerError(res, error)) return;
+        res.status(201).json(person);
       console.log('Person created successfully!');
     });
   }
   
   function putPerson(req, res) {
     const id = parseInt(req.params.id, 10)
-    const originalPerson = { firstName, lastName, nickname, from, work, school, myRelationship, weMet, relationshipDepth, birthdate, siblings, children, familyPosition, phone, email, contact, } = req.body;
+    const originalPerson = { firstName, lastName } = req.body;
     Person.findOne({ id: id}, (error, person) => {
       if (checkServerError(res, error)) return;
       if (!checkFound(res, person)) return;
   
           person.firstName = originalPerson.firstName;
           person.lastName = originalPerson.lastName;
-          person.nickname = originalPerson.nickname;
-          person.from = originalPerson.from;
-          person.work = originalPerson.work;
-          person.school = originalPerson.school;
-          person.myRelationship = originalPerson.myRelationship;
-          person.weMet = originalPerson.weMet;
-          person.relationshipDepth = originalPerson.relationshipDepth;
-          person.birthdate = originalPerson.birthdate;
-          person.siblings = originalPerson.siblings;
-          person.children = originalPerson.children;
-          person.familyPosition = originalPerson.familyPosition;
-          person.phone = originalPerson.phone;
-          person.email = originalPerson.email;
-          person.contact = originalPerson.contact;
+         
       person.save(error => {
         if (checkServerError(res, error)) return;
         res.status(200).json(person);

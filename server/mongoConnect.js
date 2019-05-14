65ca6ab6e1b3da5fbdd7628b32c578f949b3e767
Haag <kkhaag@gmail.com>
2019-05-14 15:10:40 -0600

    MongoConnect cleaner

--- server/mongoConnect.js
+++ server/mongoConnect.js
@@ -1,42 +1,25 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-  //COSMOS DB CONNECTION EXAMPLE
+  //Local Connection
   function connect() {
       mongoose.set('debug', true);
+
+      //If no db, one will be created
       const url = 'mongodb://localhost:27017/TeacherClipsx';
 
       return mongoose.connect(url, 
       { 
-          // useCreateIndex: true, 
           useNewUrlParser: true,    
       })
           .then(() => console.log('Connection to Localhost successful'))
           .catch((err) => console.error("Connection to Localhost UNSUCCESSFUL", err));
   }
 
-
-
-// var MongoClient = require('mongodb').MongoClient
-//   , assert = require('assert');
-
-// // Connection URL
-// var url = 'mongodb://localhost:27017/TeacherClip';
-// // Use connect method to connect to the Server
-// MongoClient.connect(url, function(err, db) {
-//   assert.equal(null, err);
-//   console.log("Connected correctly to server");
-
-//   db.close();
-// });
-
-
-  
   module.exports = {
     connect,
     mongoose
   };
-
 
 
   // //COSMOS DB CONNECTION EXAMPLE

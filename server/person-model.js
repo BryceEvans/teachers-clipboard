d5f6687cffc030cb2381fac0585b86e8a356e787
Haag <kkhaag@gmail.com>
2019-05-14 15:30:52 -0600

    Removed id from personmodel

--- server/person-model.js
+++ server/person-model.js
@@ -5,8 +5,6 @@ const personSchema = new Schema({
     firstName: String,
     lastName: String,
     
-    // id: { type: Number, required: true },
-    
     //Automated By Default
     updated: { type: Date, default: Date.now },
     created: { type: Date, default: Date.now },

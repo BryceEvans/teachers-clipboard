80217a1dc4cfdc6a417fe5363761ce2234b15634
Keaton Haag <codehaag@gmail.com>
2019-05-23 15:01:18 -0600

    cleanup server.js

--- server/server.js
+++ server/server.js
@@ -2,31 +2,23 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const routes = require('./routes')
-
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const routes = require('./routes')
 
 const server = express()
 
-
-server.use(bodyParser.json())
 server.use(cors())
 server.use(helmet())
 server.use(morgan('combined'))
-
-
+server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: false }))
-
-server.get('/', (req,res) => {
-    res.send("Get is Working!")
-    console.log("Get is working")
-})
 server.use('/api', routes)
 
+server.get('/', (req,res) => {
+    res.send("BE is Working!")
+})
 
-
-
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 server.listen(port, () => {console.log(`listening on ${port}`)})
